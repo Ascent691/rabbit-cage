9f768f378d7fb4781f8c3c8509308a5a85f9b9ff
Khanyisile Mathe <khanyisile.mathe@chillisoft.co.za>
2025-08-15 17:04:32 +0200

    initial work on adjusting the grid so that no two adjacent cells differ by more than 1

--- RabbitHouse/Runner/Program.cs
+++ RabbitHouse/Runner/Program.cs
@@ -6,19 +6,82 @@
         {
             var arrangements = new RabbitHouseParser().Parse(File.ReadAllLines("input.txt"));
 
-            foreach (var arrangement in arrangements)
+            var arrangement = arrangements[0];
+
+            MakeArrangementSafe(arrangement);
+
+            arrangement.Visualise();
+
+            Console.WriteLine($"Total blocks added is: {arrangement.GetTotalAddedBlocks()}");
+        }
+
+        private static void MakeArrangementSafe(RabbitHouseArrangement arrangement)
+        {
+            var hasChanges = false;
+
+            for (var row = 0; row < arrangement.TotalRows; row++)
             {
-                var heightOfCell = arrangement[0, 0];
-                arrangement.SetHeightAt(0,0, 5);
-
-                arrangement.SetHeightAt(1, 0, 5);
-                bool isSafe = arrangement.IsSafe();
-                var changed = arrangement.GetTotalAddedBlocks();
-
+                for (var col = 0; col < arrangement.TotalColumns; col++)
+                {
+                    hasChanges |= AdjustCellWithNeighbors(arrangement, row, col);
+                }
             }
 
+            if (hasChanges)
+            {
+                MakeArrangementSafe(arrangement);
+            }
+        }
 
-            Console.WriteLine("Hello, World!");
+        private static bool AdjustCellWithNeighbors(RabbitHouseArrangement arrangement, int currentRow, int currentCol)
+        {
+            var currentCellHeight = arrangement.GetHeightAt(currentRow, currentCol);
+            var hasAdjusted = false;
+
+            for (var rowOffset = -1; rowOffset <= 1; rowOffset++)
+            {
+                var neighborRow = currentRow + rowOffset;
+                if (!IsValidRow(arrangement, neighborRow)) continue;
+
+                for (var colOffset = -1; colOffset <= 1; colOffset++)
+                {
+                    var neighborCol = currentCol + colOffset;
+                    if (!IsValidColumn(arrangement, neighborCol)) continue;
+
+                    if (neighborRow == currentRow && neighborCol == currentCol) continue;
+
+                    var neighborHeight = arrangement.GetHeightAt(neighborRow, neighborCol);
+
+                    if (Math.Abs(currentCellHeight - neighborHeight) <= 1) continue;
+                    AdjustSmallerCell(arrangement, currentCellHeight, neighborHeight, currentRow, currentCol, neighborRow, neighborCol);
+                    hasAdjusted = true;
+                }
+            }
+
+            return hasAdjusted;
+        }
+
+        private static void AdjustSmallerCell(RabbitHouseArrangement arrangement, int currentCellHeight, int neighborHeight,
+            int currentRow, int currentCol, int neighborRow, int neighborCol)
+        {
+            if (currentCellHeight > neighborHeight)
+            {
+                arrangement.SetHeightAt(neighborRow, neighborCol, currentCellHeight - 1);
+            }
+            else
+            {
+                arrangement.SetHeightAt(currentRow, currentCol, neighborHeight - 1);
+            }
+        }
+
+        private static bool IsValidRow(RabbitHouseArrangement arrangement, int row)
+        {
+            return row >= 0 && row < arrangement.TotalRows;
+        }
+
+        private static bool IsValidColumn(RabbitHouseArrangement arrangement, int col)
+        {
+            return col >= 0 && col < arrangement.TotalColumns;
         }
     }
 }
