4e0687cb344c0f0a784deaf22e5ae09ab2773306
Matt Van Ryn <matthew.vanryn@chillisoft.co.za>
2025-08-15 13:24:15 +0200

    Add input and start of parser

---
+++ RabbitHouse/Runner/InputParser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Runner
+{
+    public class RabbitHouseParser
+    {
+        public RabbitHouseParser() { }
+
+        public RabbitHouse Parse(string[] lines)
+        {
+
+        }
+    }
+
+    public class RabbitHouse
+    {
+
+    }
+}
