ae72a7bab84b8bff9705a8612f5a2032d14c3949
Matt Van Ryn <matthew.vanryn@chillisoft.co.za>
2025-08-15 13:32:03 +0200

    Arrangements

--- RabbitHouse/Runner/InputParser.cs
+++ RabbitHouse/Runner/InputParser.cs
@@ -10,14 +10,42 @@ namespace Runner
     {
         public RabbitHouseParser() { }
 
-        public RabbitHouse Parse(string[] lines)
+        public RabbitHouseArrangement[] Parse(string[] lines)
         {
+            int lineIndex = 0;
+            var totalArrangements = Int32.Parse(lines[lineIndex++].Trim());
+            RabbitHouseArrangement[] result = new RabbitHouseArrangement[totalArrangements];
+            for (int i = 0; i < totalArrangements; i++) {
 
+                
+                string[] parts = lines[lineIndex++].Split(' ');
+                int numRows = Int32.Parse(parts[0]);
+                int numColumns = Int32.Parse(parts[1]);
+                int[,] cells = new int[numRows, numColumns];
+
+                for (int k = 0; k < numRows; k++)
+                {
+                    parts = lines[lineIndex++].Split(' ');
+                    for (int j = 0; j < numRows; j++)
+                    {
+                        cells[k, j] = Int32.Parse(parts[j]);
+                    }
+                }
+
+                result[i] = new RabbitHouseArrangement(cells);
+            }
+
+            return result;
         }
     }
 
-    public class RabbitHouse
+    public class RabbitHouseArrangement
     {
+        private readonly int[,] _cells;
 
+        public RabbitHouseArrangement(int[,] cells)
+        {
+            this._cells = cells;
+        }
     }
 }
--- RabbitHouse/Runner/Program.cs
+++ RabbitHouse/Runner/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            var arrangements = new RabbitHouseParser().Parse(File.ReadAllLines("input.txt"));
             Console.WriteLine("Hello, World!");
         }
     }
