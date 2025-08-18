a3e2da483bf7e4202425e0712186334df84b7910
Mark Grosvenor <mark.grosvenor@chillisoft.co.za>
2025-08-18 16:59:25 +0200

    Optimise and add answer checking

--- RabbitHouse/Runner/Program.cs
+++ RabbitHouse/Runner/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 
 namespace Runner
 {
@@ -9,114 +8,41 @@ namespace Runner
         {
             var sw = new Stopwatch();
             sw.Start();
-            
+
             var arrangements = new RabbitHouseParser().Parse(File.ReadAllLines("1.in"));
             var answers = new long[arrangements.Length];
             var currCase = 0;
 
+            var optimiser = new RabbitHouseOptimiser();
+
             foreach (var arrangement in arrangements)
             {
-                var rows = arrangement.TotalRows;
-                var cols = arrangement.TotalColumns;
-
-                var unsafeJumps = new HashSet<Jump>();
-                var changedJumps = new HashSet<Jump>();
-
-                //check all cells
-                for (var y = 0; y < rows; y++)
-                {
-                    for (var x = 0; x < cols; x++)
-                    {
-                        if (x != cols - 1)
-                        {
-                            var h = Math.Abs(arrangement[y, x] - arrangement[y, x + 1]);
-                            var jump = new Jump(new Cell(y, x), new Cell(y, x + 1));
-                            if (h > 1)
-                            {
-                                unsafeJumps.Add(jump);
-                            }
-                        }
-
-                        if (y != rows - 1)
-                        {
-                            var h = Math.Abs(arrangement[y, x] - arrangement[y + 1, x]);
-                            var jump = new Jump(new Cell(y, x), new Cell(y + 1, x));
-                            if (h > 1)
-                            {
-                                unsafeJumps.Add(jump);
-                            }
-                        }
-                    }
-                }
-
-                do
-                {
-                    //check changed jumps
-                    foreach (var changedJump in changedJumps)
-                    {
-                        var h = Math.Abs(arrangement[changedJump.A.Row, changedJump.A.Col] -
-                                         arrangement[changedJump.B.Row, changedJump.B.Col]);
-                        if (h > 1)
-                        {
-                            unsafeJumps.Add(changedJump);
-                        }
-                    }
-
-                    changedJumps.Clear();
-
-                    //make unsafe jumps safe
-                    foreach (var unsafeJump in unsafeJumps)
-                    {
-                        var hA = arrangement[unsafeJump.A.Row, unsafeJump.A.Col];
-                        var hB = arrangement[unsafeJump.B.Row, unsafeJump.B.Col];
-                        if (hA > hB)
-                        {
-                            arrangement.SetHeightAt(unsafeJump.B.Row, unsafeJump.B.Col, hA - 1);
-
-                            foreach (var jump in GetAffectedJumps(unsafeJump.B, rows, cols))
-                            {
-                                changedJumps.Add(jump);
-                            }
-                        }
-                        else
-                        {
-                            arrangement.SetHeightAt(unsafeJump.A.Row, unsafeJump.A.Col, hB - 1);
-
-                            foreach (var jump in GetAffectedJumps(unsafeJump.A, rows, cols))
-                            {
-                                changedJumps.Add(jump);
-                            }
-                        }
-                    }
-                    unsafeJumps.Clear();
-
-                } while (changedJumps.Count != 0);
-
+                optimiser.Optimise(arrangement);
                 answers[currCase++] = arrangement.GetTotalAddedBlocks();
             }
-            
-            var counter = 1;
-            var ans = answers.Select(a => $"Case #{counter++}: {a}");
-            File.WriteAllLines("answers.txt", ans);
+
+            CheckAnswers(answers);
 
             sw.Stop();
-            Console.WriteLine("Done in: " + sw.ElapsedMilliseconds);
+            Console.WriteLine($"Time: {sw.ElapsedMilliseconds} ms");
         }
 
-        private record Cell(int Row, int Col);
-
-        private record Jump(Cell A, Cell B);
-
-        private static IEnumerable<Jump> GetAffectedJumps(Cell cell, int rows, int cols)
+        private static void CheckAnswers(long[] answers)
         {
-            if (cell.Row + 1 < rows)
-                yield return new Jump(cell, cell with { Row = cell.Row + 1 });
-            if (cell.Row - 1 >= 0)
-                yield return new Jump(cell with { Row = cell.Row - 1 }, cell);
-            if (cell.Col + 1 < cols)
-                yield return new Jump(cell, cell with { Col = cell.Col + 1 });
-            if (cell.Col - 1 >= 0)
-                yield return new Jump(cell with { Col = cell.Col - 1 }, cell);
+            var correctAnswers = new RabbitHouseAnswerParser().Parse(File.ReadAllLines("1.ans"));
+
+            var incorrectAnswerCount = 0;
+            foreach (var (answer, correct) in answers.Zip(correctAnswers))
+            {
+                if (answer == correct) continue;
+
+                Console.WriteLine($"Incorrect. Expected: {correct} - Actual: {answer}");
+                incorrectAnswerCount++;
+            }
+
+            Console.WriteLine(incorrectAnswerCount == 0 
+                ? $"All {answers.Length} answers were correct" 
+                : $"{incorrectAnswerCount} answers were incorrect");
         }
     }
 }
---
+++ RabbitHouse/Runner/RabbitHouseOptimiser.cs
@@ -0,0 +1,93 @@
+﻿namespace Runner;
+
+public class RabbitHouseOptimiser
+{
+    private RabbitHouseArrangement _arrangement = null!;
+    private int _rows;
+    private int _cols;
+    private IEnumerable<Jump> _unsafeJumps = [];
+    private HashSet<Jump> _changedJumps = [];
+
+    public void Optimise(RabbitHouseArrangement arr)
+    {
+        _arrangement = arr;
+        _rows = _arrangement.TotalRows;
+        _cols = _arrangement.TotalColumns;
+
+        _unsafeJumps = GetAllUnsafeJumps();
+
+        do
+        {
+            _changedJumps = [];
+
+            foreach (var unsafeJump in _unsafeJumps)
+            {
+                MakeJumpSafe(unsafeJump);
+            }
+
+            _unsafeJumps = _changedJumps.Where(JumpIsUnsafe);
+        } while (_changedJumps.Count > 0);
+    }
+
+    private record Cell(int Row, int Col);
+
+    private record Jump(Cell A, Cell B);
+
+    private IEnumerable<Jump> GetAllUnsafeJumps()
+    {
+        for (var y = 0; y < _rows; y++)
+        {
+            for (var x = 0; x < _cols; x++)
+            {
+                if (x != _cols - 1)
+                {
+                    var h = Math.Abs(_arrangement[y, x] - _arrangement[y, x + 1]);
+                    if (h > 1)
+                        yield return new Jump(new Cell(y, x), new Cell(y, x + 1));
+                }
+
+                if (y != _rows - 1)
+                {
+                    var h = Math.Abs(_arrangement[y, x] - _arrangement[y + 1, x]);
+                    if (h > 1)
+                        yield return new Jump(new Cell(y, x), new Cell(y + 1, x));
+                }
+            }
+        }
+    }
+
+    private bool JumpIsUnsafe(Jump jump)
+    {
+        var h = Math.Abs(_arrangement[jump.A.Row, jump.A.Col] -
+                         _arrangement[jump.B.Row, jump.B.Col]);
+        return h > 1;
+    }
+
+    private void MakeJumpSafe(Jump unsafeJump)
+    {
+        var hA = _arrangement[unsafeJump.A.Row, unsafeJump.A.Col];
+        var hB = _arrangement[unsafeJump.B.Row, unsafeJump.B.Col];
+        if (hA > hB)
+        {
+            _arrangement.SetHeightAt(unsafeJump.B.Row, unsafeJump.B.Col, hA - 1);
+            _changedJumps.UnionWith(GetAffectedJumps(unsafeJump.B));
+        }
+        else
+        {
+            _arrangement.SetHeightAt(unsafeJump.A.Row, unsafeJump.A.Col, hB - 1);
+            _changedJumps.UnionWith(GetAffectedJumps(unsafeJump.A));
+        }
+    }
+
+    private IEnumerable<Jump> GetAffectedJumps(Cell cell)
+    {
+        if (cell.Row + 1 < _rows)
+            yield return new Jump(cell, cell with { Row = cell.Row + 1 });
+        if (cell.Row - 1 >= 0)
+            yield return new Jump(cell with { Row = cell.Row - 1 }, cell);
+        if (cell.Col + 1 < _cols)
+            yield return new Jump(cell, cell with { Col = cell.Col + 1 });
+        if (cell.Col - 1 >= 0)
+            yield return new Jump(cell with { Col = cell.Col - 1 }, cell);
+    }
+}
