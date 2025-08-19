0ac877db0a37defec44323f49456656b2f76b87f
Brendon Page <brendon.page@chillisoft.co.za>
2025-08-19 22:44:15 +0400

    Refactored and added some TODOs.

--- RabbitHouse/Runner/Program.cs
+++ RabbitHouse/Runner/Program.cs
@@ -1,91 +1,98 @@
 ﻿using System.Diagnostics;
 
-namespace Runner
+// TODO get rid of need for an additional loop to reference neighbors by giving the cell a reference to all cells
+//      and the dimensions of the grid.
+// TODO move the creation and population of the queue into the parser, which should put it onto RabbitHouseArrangement,
+//      the Cells property can then be removed.
+// TODO draw algorithm diagram.
+// TODO try to make code more readable, and where that is not possible document performance choices
+//      and give "should consider using in production" rating.
+
+namespace Runner;
+
+internal static class Program
 {
-    internal static class Program
+    private static readonly string InputPath = "1.in";
+    private static readonly string AnswerPath = "1.ans";
+
+    static async Task Main()
     {
-        private static readonly string InputPath = "1.in";
-        private static readonly string AnswerPath = "1.ans";
+        var solutionStopWatch = Stopwatch.StartNew();
+        var arrangements = new RabbitHouseParser().Parse(InputPath);
 
-        static async Task Main()
-        {
-            var solutionStopWatch = Stopwatch.StartNew();
-            var arrangements = new RabbitHouseParser().Parse(InputPath);
+        await using var output = new StreamWriter(Console.OpenStandardOutput());
 
-            await using var output = new StreamWriter(Console.OpenStandardOutput());
+        var answers = await CalculateAnswers(arrangements, output);
 
-            var answers = await CalculateAnswers(arrangements, output);
+        solutionStopWatch.Stop();
+        await output.WriteLineAsync($"Solution Time (Including Console Output) : {solutionStopWatch.Elapsed.ToString()}");
 
-            solutionStopWatch.Stop();
-            await output.WriteLineAsync($"Solution Time (Including Console Output) : {solutionStopWatch.Elapsed.ToString()}");
-
-            var answerVerificationStopWatch = VerifyAnswers(answers, output);
-            await output.WriteLineAsync($"Answer Verification Time                 : {answerVerificationStopWatch.Elapsed.ToString()}");
+        var answerVerificationStopWatch = VerifyAnswers(answers, output);
+        await output.WriteLineAsync($"Answer Verification Time                 : {answerVerificationStopWatch.Elapsed.ToString()}");
             
-            var totalTime = answerVerificationStopWatch.Elapsed + solutionStopWatch.Elapsed;
-            await output.WriteLineAsync($"Total Time                               : {totalTime.ToString()}");
-        }
+        var totalTime = answerVerificationStopWatch.Elapsed + solutionStopWatch.Elapsed;
+        await output.WriteLineAsync($"Total Time                               : {totalTime.ToString()}");
+    }
 
-        private static Task<Answer[]> CalculateAnswers(RabbitHouseArrangements arrangements, StreamWriter output)
+    private static Task<Answer[]> CalculateAnswers(RabbitHouseArrangements arrangements, StreamWriter output)
+    {
+        return Task.Run(() =>
         {
-            return Task.Run(() =>
+            arrangements.WaitForTotalNumberOfArrangementsToBeRead();
+                
+            if(arrangements.Data == null) throw new Exception("Oh noes you flew to close to the sun!!!");
+
+            var answers = new Answer[arrangements.Data.Length];
+                
+            for (var i = 0; i < arrangements.Data.Length; i++)
             {
-                arrangements.WaitForTotalNumberOfArrangementsToBeRead();
-                
-                if(arrangements.Data == null) throw new Exception("Oh noes you flew to close to the sun!!!");
+                arrangements.WaitArrangementDataToBeRead();
+                var arrangement = arrangements.Data[i];
 
-                var answers = new Answer[arrangements.Data.Length];
-                
-                for (var i = 0; i < arrangements.Data.Length; i++)
+                var answer = new Answer(i);
+                answers[i] = answer;
+
+                var cells = arrangement.Cells;
+                var queue = new CellQueue();
+                    
+                for (var cellRow = 0; cellRow < cells.GetLength(0); cellRow++)
+                for (var cellColumn = 0; cellColumn < cells.GetLength(1); cellColumn++)
                 {
-                    arrangements.WaitArrangementDataToBeRead();
-                    var arrangement = arrangements.Data[i];
-
-                    var answer = new Answer(i);
-                    answers[i] = answer;
-
-                    var cells = arrangement.Cells;
-                    var queue = new CellQueue();
-                    
-                    for (var cellRow = 0; cellRow < cells.GetLength(0); cellRow++)
-                    for (var cellColumn = 0; cellColumn < cells.GetLength(1); cellColumn++)
-                    {
-                        var cell = cells[cellRow, cellColumn];
-                        cell.ReferenceNeighbours(cells, arrangement.TotalRows, arrangement.TotalColumns);
-                        queue.EnqueueNonZero(cell);
-                    }
-
-                    while (queue.Head is not null)
-                    {
-                        var cell = queue.Dequeue();
-                        if (cell is not null)
-                        {
-                            answer.Count += cell.MakeSafe(queue);    
-                        }
-                    }
-                    
-                    output.WriteLine(answer.ToString());
+                    var cell = cells[cellRow, cellColumn];
+                    cell.ReferenceNeighbours(cells, arrangement.TotalRows, arrangement.TotalColumns);
+                    queue.EnqueueNonZero(cell);
                 }
 
-                return answers;
-            });
-        }
-
-        private static Stopwatch VerifyAnswers(Answer[] calculatedAnswers, StreamWriter o)
-        {
-            var answerVerificationStopWatch = Stopwatch.StartNew();
-            
-            var actualAnswers = File.ReadAllLines(AnswerPath);
-            for (var i = 0; i < calculatedAnswers.Length; i++)
-            {
-                if (calculatedAnswers[i].ToString() != actualAnswers[i])
+                while (queue.Head is not null)
                 {
-                    o.WriteLine($"Difference detected, calculated: '{calculatedAnswers[i]}', answer: '{actualAnswers[i]}'");
+                    var cell = queue.Dequeue();
+                    if (cell is not null)
+                    {
+                        answer.Count += cell.MakeSafe(queue);    
+                    }
                 }
+                    
+                output.WriteLine(answer.ToString());
             }
-            answerVerificationStopWatch.Stop();
+
+            return answers;
+        });
+    }
+
+    private static Stopwatch VerifyAnswers(Answer[] calculatedAnswers, StreamWriter o)
+    {
+        var answerVerificationStopWatch = Stopwatch.StartNew();
             
-            return answerVerificationStopWatch;
+        var actualAnswers = File.ReadAllLines(AnswerPath);
+        for (var i = 0; i < calculatedAnswers.Length; i++)
+        {
+            if (calculatedAnswers[i].ToString() != actualAnswers[i])
+            {
+                o.WriteLine($"Difference detected, calculated: '{calculatedAnswers[i]}', answer: '{actualAnswers[i]}'");
+            }
         }
+        answerVerificationStopWatch.Stop();
+            
+        return answerVerificationStopWatch;
     }
 }
