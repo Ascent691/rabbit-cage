a3c9a9e37a5b95cddc113a921e1c0cf9b96d5f9a
Brendon Page <brendon.page@chillisoft.co.za>
2025-08-20 21:29:36 +0400

    Minor renaming.

--- RabbitHouse/Runner/RabbitHouseParser.cs
+++ RabbitHouse/Runner/RabbitHouseParser.cs
@@ -16,10 +16,10 @@
             {
                 ReadOnlySpan<byte> data = File.ReadAllBytes(path);
                 
-                int position = 0;
+                int offset = 0;
 
                 var totalArrangementsLineValues = new int[1];
-                CopyNumbersOnLineTo(data, ref position, totalArrangementsLineValues);
+                CopyNumbersOnLineTo(data, ref offset, totalArrangementsLineValues);
                 var totalArrangements = totalArrangementsLineValues[0];
                 
                 rabbitHouseArrangements.Data = new RabbitHouseArrangement[totalArrangements];
@@ -29,7 +29,7 @@
                 
                 for (int i = 0; i < totalArrangements; i++)
                 {
-                    CopyNumbersOnLineTo(data, ref position, sizeLineValues);
+                    CopyNumbersOnLineTo(data, ref offset, sizeLineValues);
                     
                     var numRows = sizeLineValues[0];
                     var numColumns = sizeLineValues[1];
@@ -41,7 +41,7 @@
 
                     for (int row = 0; row < numRows; row++)
                     {
-                        CopyNumbersOnLineTo(data, ref position, dataLineValues);
+                        CopyNumbersOnLineTo(data, ref offset, dataLineValues);
                         for (int column = 0; column < numColumns; column++)
                         {
                             var cell = new Cell(row, column, dataLineValues[column], cells);
@@ -61,28 +61,28 @@
             return rabbitHouseArrangements;
         }
         
-        private static void CopyNumbersOnLineTo(ReadOnlySpan<byte> span, ref int position, int[] destination)
+        private static void CopyNumbersOnLineTo(ReadOnlySpan<byte> source, ref int offset, int[] destination)
         {
             for (var i = 0; i < destination.Length; i++)
             {
                 int parsedValue = 0;
-                while (span[position] != Space && span[position] != CarriageReturn  && span[position] != NewLine)
+                while (source[offset] != Space && source[offset] != CarriageReturn  && source[offset] != NewLine)
                 {
-                    var digit = span[position] - 48;
+                    var digit = source[offset] - 48;
                     parsedValue = parsedValue * 10 + digit;
-                    position++;
+                    offset++;
                 }
                 destination[i] = parsedValue;
                 
-                while (span[position] == Space)
+                while (source[offset] == Space)
                 {
-                    position++;
+                    offset++;
                 }
             }
             
-            while (position < span.Length && (span[position] == CarriageReturn || span[position] == NewLine))
+            while (offset < source.Length && (source[offset] == CarriageReturn || source[offset] == NewLine))
             {
-                position++;
+                offset++;
             }
         }
     }
